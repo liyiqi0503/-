c4248530dd5c886e4cd6f98b8216fb9ce5426175
GoodNghit <97924914+GoodNghit@users.noreply.github.com>
2023-01-18 17:52:03 +0800

    Add files via upload

---
+++ LOGIN/js/Jindex.js
@@ -0,0 +1,12 @@
+function turnTomain(){
+	
+	var string=document.getElementById("user").value;
+	var string2=document.getElementById("pwd").value;
+	if(string=="p"&&string2=="123"){
+		window.location.href='旋转变色效果.html';
+	}
+	else{
+		window.alert("no");
+	}
+	
+}
